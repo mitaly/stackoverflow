76aeb45a764f574730bc391ef924d6958523d801
Mitaly <senmitaly13@gmail.com>
2024-08-24 13:21:17 +0530

    Update AnswerRepository.java

--- src/main/java/com/craft/stackoverflow/repository/AnswerRepository.java
+++ src/main/java/com/craft/stackoverflow/repository/AnswerRepository.java
@@ -2,7 +2,9 @@ package com.craft.stackoverflow.repository;
 
 import com.craft.stackoverflow.entities.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnswerRepository  extends JpaRepository<Answer, Long> {
 
 }
