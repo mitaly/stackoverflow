cb373d79e5e0492c50964ce751bcb552af5ef947
mitaly <senmitaly13@gmail.com>
2024-08-27 12:25:18 +0530

    added vote api for post

--- src/main/java/com/craft/stackoverflow/controller/PostController.java
+++ src/main/java/com/craft/stackoverflow/controller/PostController.java
@@ -2,13 +2,14 @@ package com.craft.stackoverflow.controller;
 
 import com.craft.stackoverflow.dto.PostDto;
 import com.craft.stackoverflow.entities.Post;
+import com.craft.stackoverflow.entities.User;
+import com.craft.stackoverflow.entities.VoteType;
 import com.craft.stackoverflow.service.PostService;
+import com.craft.stackoverflow.service.VoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/posts")
@@ -17,10 +18,20 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private VoteService voteService;
+
     @GetMapping("{postId}")
     public ResponseEntity<PostDto> getPost(@PathVariable Long postId) {
         return ResponseEntity.ok(postService.getPost(postId));
     }
-    
+
+    @PostMapping("{postId}")
+    public ResponseEntity<PostDto> votePost(@PathVariable Long postId,
+                                            @RequestParam("voteType") VoteType voteType,
+                                            @AuthenticationPrincipal User user){
+        return ResponseEntity.ok(voteService.addVote(postId, voteType, user));
+    }
+
 
 }
--- src/main/java/com/craft/stackoverflow/service/VoteService.java
+++ src/main/java/com/craft/stackoverflow/service/VoteService.java
@@ -1,9 +1,12 @@
 package com.craft.stackoverflow.service;
 
+import com.craft.stackoverflow.dto.PostDto;
 import com.craft.stackoverflow.dto.QuestionDTO;
 import com.craft.stackoverflow.entities.*;
 import com.craft.stackoverflow.exception.BusinessException;
+import com.craft.stackoverflow.mapper.PostMapper;
 import com.craft.stackoverflow.repository.PostRepository;
+import com.craft.stackoverflow.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -18,46 +21,52 @@ public class VoteService {
     @Autowired
     private PostRepository postRepository;
 
-    public Post addVote(Long postId, VoteType voteType, User user) {
-        Optional<Post> post = postRepository.findById(postId);
-        return post.get();
-//        if (question.isEmpty()) {
-//            throw new BusinessException(HttpStatus.NOT_FOUND.value(), "question.not.found", questionId);
-//        }
-//        Question question1 = question.get();
-//        Optional<Vote> usersVote = question1.
-//                getVotes()
-//                .stream()
-//                .filter(upVote ->
-//                        Objects.equals(upVote.getUser().getId(), user.getId()))
-//                .findFirst();
-//
-//        //user has not upvoted/downvoted the question.
-//        if (usersVote.isEmpty()) {
-//            Vote vote = new Vote();
-//            vote.setUser(user);
-//            vote.setVoteType(voteType);
-//            vote.setPost(question1);
-//            vote = voteRepository.save(vote);
-//            List<Vote> voteList = question1.getVotes();
-//            voteList.add(vote);
-//            question1.setVotes(voteList);
-//        } else {
-//            Vote vote = usersVote.get();
-//            if(vote.getVoteType() == voteType) {
-//                voteRepository.delete(vote);
-//                List<Vote> voteList = question1.getVotes();
-//                voteList.remove(vote);
-//            }else{
-//                List<Vote> voteList = question1.getVotes();
-//                voteList.remove(vote);
-//                vote.setVoteType(voteType);
-//                voteList.add(vote);
-//                question1.setVotes(voteList);
-//                voteRepository.save(vote);
-//            }
-//        }
-//        return questionMapper.questionToQuestionDTO(question1);
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private PostMapper postMapper;
+
+    public PostDto addVote(Long postId, VoteType voteType, User user) {
+        Optional<Post> postOptional = postRepository.findById(postId);
+
+        if (postOptional.isEmpty()) {
+            throw new BusinessException(HttpStatus.NOT_FOUND.value(), "post.not.found", postId);
+        }
+        Post post = postOptional.get();
+        Optional<Vote> usersVote = post.
+                getVotes()
+                .stream()
+                .filter(upVote ->
+                        Objects.equals(upVote.getUser().getId(), user.getId()))
+                .findFirst();
+
+        //user has not upvoted/downvoted the question.
+        if (usersVote.isEmpty()) {
+            Vote vote = new Vote();
+            vote.setUser(user);
+            vote.setVoteType(voteType);
+            vote.setPost(post);
+            vote = voteRepository.save(vote);
+            List<Vote> voteList = post.getVotes();
+            voteList.add(vote);
+            post.setVotes(voteList);
+        } else {
+            Vote vote = usersVote.get();
+            if(vote.getVoteType() == voteType) {
+                voteRepository.delete(vote);
+                List<Vote> voteList = post.getVotes();
+                voteList.remove(vote);
+            }else{
+                List<Vote> voteList = post.getVotes();
+                voteList.remove(vote);
+                vote.setVoteType(voteType);
+                voteList.add(vote);
+                post.setVotes(voteList);
+                voteRepository.save(vote);
+            }
+        }
+        return postMapper.postToPostDto(post);
 
 
     }
